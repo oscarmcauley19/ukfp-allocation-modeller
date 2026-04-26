d93749cd70f9b136ad70e48daa91d90d101aad5a
Oscar McAuley <oscarmcauley19@gmail.com>
2026-04-26 19:49:38 +0100

    Update frontend bucket access control

--- infrastructure/modules/frontend/main.tf
+++ infrastructure/modules/frontend/main.tf
@@ -5,6 +5,15 @@ resource "aws_s3_bucket" "frontend" {
 resource "aws_s3_bucket_acl" "frontend_acl" {
   bucket = aws_s3_bucket.frontend.id
   acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership_controls" {
+  bucket = aws_s3_bucket.frontend.id
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
